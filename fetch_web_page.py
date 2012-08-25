d6471eaf1a5ded265f5635c9da5df400aca967e7
chen.liang <chenliangcareer@gmail.com>
2012-08-25 18:13:10 +0800

    Nexus 7, i am waiting for you

---
+++ fetch_web_page.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import urllib,sys,smtplib,poplib,time
+
+
+smtpServer = "smtp.gmail.com:587"
+fromAddress = "chenliangcareer@gmail.com"
+toAddress = "chenliangcareer@gmail.com"
+
+msgHead = ['From:chenliangcareer@gmail.com', 'To:chenliangcareer@gmail.com', 'Subject:Nexus 7 being delivered']
+msgBody = ['Nexus 7 being delivered']
+msg = '\r\n\r\n'.join(['\r\n'.join(msgHead), '\r\n'.join(msgBody)])
+
+def notifyMe():
+    s = smtplib.SMTP(smtpServer)
+    s.starttls()
+    s.login("chenliangcareer@gmail.com", "!lp8ssword!")
+    s.sendmail(fromAddress, toAddress, msg)
+    s.quit()
+
+while True:
+    a=urllib.urlopen("http://www.culexpress.com/CulBill.aspx?wlnum=CUL000078925")
+    b=a.read()
+    #print b
+    b.decode("utf8")
+    #print b
+    c=b.index("<tbody>")
+    #print c
+    #print c
+    d=b.index("</tbody>")
+    #d+=1
+    if d != 6099:
+        notifyMe()
+        break
+    else:
+        print "not yet"
+
+    time.sleep(5)
