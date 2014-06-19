b51bb17c1ee35771432a7f2b85908c26f49eedb9
陈亮 <chenliang.info@gmail.com>
2014-06-19 12:58:59 +0800

    update stats scripts

--- parson_json.py
+++ parson_json.py
@@ -1,10 +1,22 @@
 import sys
-
+import json
 # __author__ = 'chenliang'
 
-with open(sys.argv[1], 'r') as f:
-    print "------------------------------"
-    read_data = f.read()
-    print read_data
-f.closed
+file = open(sys.argv[1], 'r')
+
+for line in file.readlines():
+    decoded = json.loads(line)
+    sequenceCount = len(decoded)
+    for index in range(sequenceCount):
+        # "taskIds":[183],"knowledgeId":50
+        item = decoded[index];
+        taskIds = item['taskIds'];
+        assert len(taskIds) == 1
+        # print "sequenceId=", item['sequenceId'], " knowledgeId=",item['knowledgeId'], " taskIds=", item['taskIds'], taskIds[0]
+        print item['knowledgeId'], taskIds[0]
+file.close()
+
+# print sys.argv[1]
+
+
 
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+__author__ = 'chenliang'
+
+print sys.argv[1], sys.args
