9e1335c75d8bdfd664976aaa48ed9b987e2f6c97
Liang <chenliang.info@gmail.com>
2014-07-07 11:14:29 +0800

    批量生成数据

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,4 @@
+rm *.xls
 #sh run.sh 16
 #sh run.sh 17
 #sh run.sh 18
@@ -5,5 +6,11 @@
 #sh run.sh 20 
 #sh run.sh 21 
 #ssh run.sh 22 
-sh run.sh 23 
-
+#sh run.sh 24 
+#sh run.sh 25 
+#sh run.sh 26 
+for mission in `seq 11 26`
+do
+    sh run.sh $mission
+done
+#echo "" | mutt -a "`ls *.xls`" -s "统计数据" -- huangxin@accurator.com.cn tuxiaolei@accurator.com.cn linxiao@accurator.com.cn zhuyanbin@accurator.com.cn chenliang@accurator.com.cn
