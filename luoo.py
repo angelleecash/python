c49f591d6e89b5deb580d77712b62b571654bf58
陈亮 <chenliang.info@gmail.com>
2014-08-23 11:46:09 +0800

    using multithread to process all categories

--- luoo.py
+++ luoo.py
@@ -1,15 +1,10 @@
 # __author__ = 'chenliang'
 import urllib2
-
-print """Content-Type: text/html\n"""
-print "<html>"
-print "<body>"
-
+import threading
 
 def parseLink(line):
     # l = '<a href="http://www.luoo.net/music/229" class="name" title="Leap Of Faith">vol.229 Leap Of Faith</a>'
     l = line
-
 
     hrefStart = l.index('"') + 1
     hrefEnd = l.index('"', hrefStart)
@@ -36,14 +31,13 @@ def comp(x, y):
 
 categories = ["folk", "metal"]
 
-for category in categories:
+def processCategory(category):
     guessPageCount = 20
-
     allItems = []
-    print "<table border=\"1\">"
+    # print "<table border=\"1\">"
 
     # url = "http://www.luoo.net/music/folk"
-    for page in range(1,20):
+    for page in range(1, guessPageCount):
         url = "http://www.luoo.net/tag/" + category + "?p=" + str(page)
         response = urllib2.urlopen(url)
         html = response.read()
@@ -77,26 +71,90 @@ for category in categories:
                         lineno += 1
                         currentItem.append(line)
 
-
-
-
-    if len(allItems) <= 0:
-        continue
-
     # print "---->", category
 
     # allItems.sort(cmp=lambda x,y : cmp(x[7], y[7]))
     allItems.sort(cmp=comp, reverse=True)
 
-    print "<tr>"
+    # print "<tr>"
+
+    result = []
+
     for i in range(0, min(len(allItems), 10)):
         link = allItems[i][0]
         like = allItems[i][7]
         title, href = parseLink(link)
-        print "<td><a href=\""+ href+ "\">"+ title + "</a></td>"
-    print "</tr>"
+        result.append([title, like, href])
 
-    print "</table>"
+    return result
+        # print "<td><a href=\""+ href+ "\">"+ title + "</a></td>"
+    # print "</tr>"
+
+results = []
+
+for category in categories:
+    t = threading.Thread(target=processCategory, args=(category))
+    t.start()
+    t.join()
+    # guessPageCount = 20
+    #
+    # allItems = []
+    # print "<table border=\"1\">"
+    #
+    # for page in range(1,20):
+    #     url = "http://www.luoo.net/tag/" + category + "?p=" + str(page)
+    #     response = urllib2.urlopen(url)
+    #     html = response.read()
+    #     htmlContent = str(html).split("\n")
+    #     count = 0
+    #     matched = False
+    #     lineno = 1
+    #
+    #     currentItem = []
+    #
+    #     for line in htmlContent:
+    #         count += 1
+    #         line = line.lstrip()
+    #         line = line.rstrip()
+    #
+    #         if line == '<div class="meta rounded clearfix">':
+    #             matched = True
+    #             lineno = 1
+    #             currentItem = []
+    #         else:
+    #             if line == '</div>':
+    #                 if matched:
+    #                     matched = False
+    #                     # print currentItem
+    #                     if len(currentItem) > 0:
+    #                         allItems.append(currentItem)
+    #                     currentItem = []
+    #             else:
+    #                 if matched :
+    #                     # print lineno, ":", line
+    #                     lineno += 1
+    #                     currentItem.append(line)
+    #
+    #
+    #
+    #
+    # if len(allItems) <= 0:
+    #     continue
+    #
+    # # print "---->", category
+    #
+    # # allItems.sort(cmp=lambda x,y : cmp(x[7], y[7]))
+    # allItems.sort(cmp=comp, reverse=True)
+    #
+    # print "<tr>"
+    # for i in range(0, min(len(allItems), 10)):
+    #     link = allItems[i][0]
+    #     like = allItems[i][7]
+    #     title, href = parseLink(link)
+    #     print "<td><a href=\""+ href+ "\">"+ title + "</a></td>"
+    # print "</tr>"
+    #
+    # print "</table>"
         # print title, "(", str(like) ,")" #, "-->",href
     # for item in allItems:
     #     link = item[0]
@@ -108,6 +166,9 @@ for category in categories:
     # print "\n\n\n"
 
 
+print """Content-Type: text/html\n"""
+print "<html>"
+print "<body>"
 
 
 print "</body>"
