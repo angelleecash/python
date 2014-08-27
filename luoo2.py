31e2cadd5dffa8587631cce65b0a274d6f0ff421
Liang <chenliangcareer@gmail.com>
2014-08-27 14:30:08 +0800

    change encoding to utf8

--- luoo2.py
+++ luoo2.py
@@ -28,8 +28,11 @@ for line in htmlContent:
 
 print """Content-Type: text/html\n"""
 print "<html>"
+print "<head>"
+print "<meta charset=\"UTF-8\">"
+print "</head>"
 print "<body>"
-
+print "shit"
 print "<table>"
 
 for result in results:
